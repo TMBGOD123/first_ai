72ba1ef12769bcaddc37c44ee4a65158c7898edf
TMBGOD123 <TishkaXd2@gmail.com>
2026-02-27 03:32:21 +0400

    загрузка датасета

---
+++ first_ai.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+print(x_train.shape)
+print(x_test.shape)
